af498cfcae70bf65b6480d4c9933afa160a7101d
Richard <spike8884@gmail.com>
2024-06-14 08:52:39 -0400

    updated repo

---
+++ backend/app.js
@@ -0,0 +1,79 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const app = express();
+const cors = require("cors");
+require("dotenv").config();
+
+// middleware
+const corsOptions = {
+    origin: "http://localhost:3000" // frontend URI (ReactJS)
+}
+app.use(express.json());
+app.use(cors(corsOptions));
+
+// connect MongoDB
+mongoose.connect(process.env.MONGODB_URI).then(() => {
+    const PORT = process.env.PORT || 8000
+    app.listen(PORT, () => {
+        console.log(`App is Listening on PORT ${PORT}`);
+    })
+}).catch(err => {
+    console.log(err);
+});
+
+
+
+import { useEffect, useState } from "react";
+
+function App() {
+  const [message, setMessage] = useState("");
+
+  // Fetching message from backend on mount
+  useEffect(() => {
+    fetch("http://localhost:4000")
+      .then((res) => res.json())
+      .then((data) => setMessage(data.message));
+  }, []);
+
+  return (
+    <div className="App">
+      <h1>{message}</h1>
+    </div>
+  );
+}
+
+export default App;
+
+
+
+// route
+app.get("/", (req, res) => {
+    res.status(201).json({message: "Connected to Backend!"});
+    const { MongoClient, ServerApiVersion } = require('mongodb');
+    const uri = "mongodb+srv://spike8884:carmen22@cluster0.dmfrcwn.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+    // Create a MongoClient with a MongoClientOptions object to set the Stable API version
+    const client = new MongoClient(uri, {
+      serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+      }
+    });
+    async function run() {
+      try {
+        // Connect the client to the server	(optional starting in v4.7)
+        await client.connect();
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+      } finally {
+        // Ensures that the client will close when you finish/error
+        await client.close();
+      }
+    }
+    run().catch(console.dir);
+
+
+
+
+});
